34052fa014b4de747ce43d21f5a80d81a3460c92
Daniel Messori <messoridaniel@gmail.com>
2026-03-13 16:51:13 -0400

    update next s

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
-import createNextPwa from 'next-pwa'
-
-const isProd = process.env.NODE_ENV === 'production'
-
-const withPWA = isProd
-  ? createNextPwa({
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      // Puedes personalizar esto para limitar el scope a `/admin`
-      scope: '/admin',
-    })
-  : (config) => config
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // Obligamos a usar Webpack si estamos usando next-pwa
-  ...(isProd && {
-    webpack: (config) => config,
-  }),
+  output: "standalone"
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
